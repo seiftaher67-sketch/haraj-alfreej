786a3d574bff81d7a54d7d1f4879c431bc4d394d
salmamohamed21 <salmamohamed0266@gmail.com>
2025-11-01 22:11:18 +0200

    Update LiveBroadcast.js

--- src/pages/LiveBroadcast.js
+++ src/pages/LiveBroadcast.js
@@ -135,11 +135,10 @@ function LiveBroadcast() {
 
           <div className="text-center mt-12">
             <div className="flex items-center justify-center gap-4 mb-6">
-              <img src="/assets/images/icons/logos_paypal.png" alt="paypal" className="h-8" />
+              <img src="/assets/images/icons/2560px-Mada_Logo.svg 1.png" alt="paypal" className="h-8" />
               <img src="/assets/images/icons/logos_visa.png" alt="visa" className="h-8" />
               <img src="/assets/images/icons/icons.png" alt="mastercard" className="h-8" />
-              <img src="/assets/images/icons/fa-brands_apple-pay.png" alt="applepay" className="h-8" />
-            </div>
+             </div>
             
           </div>
         </div>
